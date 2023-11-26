9e6615e2a0a30091d5e43536624e7917cad73ac6
Brayan Guevara <bguevaravan@gmail.com>
2023-11-25 19:25:48 -0500

    Arhivos iniciales de electron

---
+++ main.js
@@ -0,0 +1,33 @@
+// Importa las clases 'app' y 'BrowserWindow' desde el módulo 'electron'
+const { app, BrowserWindow } = require('electron')
+
+// Incluye el módulo 'path' de Node.js al principio de tu archivo
+const path = require('node:path')
+// Función para crear una nueva ventana
+const createWindow = () => {
+    // Crea una nueva instancia de BrowserWindow
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600,    
+      webPreferences: {
+    // Especifica el script de preload para configurar el entorno de ejecución del navegador
+      preload: path.join(__dirname, 'preload.js')
+      }
+    })
+
+   // Carga el archivo 'index.html' en la ventana
+    win.loadFile('index.html')
+  }
+// Ejecuta la función createWindow cuando la aplicación está lista
+  app.whenReady().then(() => {
+    createWindow()
+    // Maneja el evento 'activate', que se dispara cuando se activa la aplicación
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+        
+      })
+  })
+  // Maneja el evento 'window-all-closed', que se dispara cuando todas las ventanas están cerradas
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+  })
